d1663c32262c5c130a38e1ca588ea3359a233b57
fati788 <fela102@g.educaand.es>
2026-02-10 19:31:52 +0100

    first commit

---
+++ src/main/java/com/jaroso/proyectointermodular2026/ProyectoIntermodular2026Application.java
@@ -0,0 +1,13 @@
+package com.jaroso.proyectointermodular2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntermodular2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIntermodular2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaroso/proyectointermodular2026/ProyectoIntermodular2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaroso.proyectointermodular2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntermodular2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
