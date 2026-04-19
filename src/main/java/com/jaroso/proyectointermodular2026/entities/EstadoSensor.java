d4874ad394adc3e4cab2768a1b8d9a57084b6574
fati788 <fela102@g.educaand.es>
2026-04-19 19:40:49 +0200

    commit

--- src/main/java/com/jaroso/proyectointermodular2026/entities/EstadoSensor.java
+++ src/main/java/com/jaroso/proyectointermodular2026/entities/EstadoSensor.java
@@ -1,5 +1,11 @@
 package com.jaroso.proyectointermodular2026.entities;
 
 public enum EstadoSensor {
-    ACTIVO,INACTIVO,DESCONECTADO,ERROR,MANTENIMIENTO
+    ACTIVO,          // funcionando y enviando datos
+    INACTIVO,        // existe pero no envía
+    DESCONECTADO,    // no hay comunicación
+    ERROR,           // fallo de hardware o lectura
+    MANTENIMIENTO,
+    ARRANCADO,
+    PARADO
 }
